72d63b01f03df979305dde861187d812d6f73f9f
JonKopp99 <jonkopp99@gmail.com>
2019-03-01 23:47:28 -0800

    Updated Mood tracker.

--- BigMood/View Controllers/moodTracker.swift
+++ BigMood/View Controllers/moodTracker.swift
@@ -106,6 +106,7 @@ class moodTracker: UIViewController{
     }
     func getColorSet() -> [UIColor]
     {
+        //["Happy","Bored","Frustrated","Angry","Lonely","Sad"]
         let colors = [#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1),#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1),#colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1),#colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1),#colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1),#colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)]
         var theColors = [UIColor]()
         for i in theMoods
